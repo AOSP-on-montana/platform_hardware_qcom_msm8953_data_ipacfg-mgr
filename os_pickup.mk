c24eaa01522c3f3b8c1719ac5d3c7c34f04801ac
Alex Hong <rurumihong@google.com>
2019-05-15 20:21:31 +0800

    skip building when BUILD_WITHOUT_VENDOR = true
    Bug: 128542475
    Bug: 129245318
    Change-Id: I96804ca004b4feb576294af9b4981624b68cf346

--- os_pickup.mk
+++ os_pickup.mk
@@ -1,4 +1,6 @@
 ifneq ($(filter msm8937 msm8953,$(PRODUCT_PLATFORM)),)
+ifneq ($(BUILD_WITHOUT_VENDOR), true)
 LOCAL_PATH := $(call my-dir)
 include $(call first-makefiles-under,$(LOCAL_PATH))
+endif
 endif
